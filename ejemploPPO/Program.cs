29f888881e13fb2958151de793e45fe0d2b5724b
Francisco José Díaz <74576885+Fridnansen@users.noreply.github.com>
2020-11-28 13:28:45 -0300

    Add files via upload

---
+++ ejemploPPO/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ejemploPPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo miCirculo; // creación de objeto de tipo Circulo. Variable / objeto de tipo circulo 
+
+            miCirculo = new Circulo(); //iniciación de variable /objeto de tipo Circulo. Instanciar una clase
+            // Intanciación. Ejemplarizar. Creación de ejemplar de clase
+
+            Console.WriteLine(miCirculo.CalculoArea(5));
+
+            Circulo miCirculo2 = new Circulo();
+
+            Console.WriteLine(miCirculo2.CalculoArea(9));
+
+            Console.WriteLine(miCirculo.CalculoArea(5));
+
+            CoversorEuroDolar obj = new CoversorEuroDolar();
+            
+            obj.cambiaValorEuro(-1.45);
+
+            Console.WriteLine(obj.convierte(50));
+        }
+    }
+
+    class Circulo
+    {
+
+        private const double pi = 3.1416; // propiedad de la clase circulo. Campo de Clase
+        public double CalculoArea(int radio) //metodo de clase ¿Que puede hacer los objetos de tipo circulo?
+        {
+            return pi * radio * radio;
+        }
+
+    }
+
+    class CoversorEuroDolar
+    {
+
+        private double euro = 1.253;
+
+        public double convierte(double cantidad)
+        {
+
+            return cantidad * euro;
+
+        }
+
+
+
+        public void cambiaValorEuro(double nuevoValor)
+        {
+            if (nuevoValor < 0) euro = 1.253;
+
+            else
+            euro = nuevoValor;
+
+        }
+    }
+}
---
+++ usocoche30/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace usocoche30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Coche coche1 = new Coche(); //crear objeto/instancia de tipo coche
+            // dar un estado inicial a nuestro coche
+            Coche coche2 = new Coche();
+
+            //coche2 getRuedas = 27;
+
+            Console.WriteLine(coche1.getInfoCoche());
+            Console.WriteLine(coche2.getInfoCoche());
+            Console.WriteLine(coche2.getExtras());
+            Coche coche3 = new Coche(4500.25, 1200.35);
+
+            Console.WriteLine(coche3.getInfoCoche());
+            coche3.setExtras(true, "cuero");
+            Console.WriteLine(coche3.getExtras());
+        }
+    
+    }
+    partial class Coche
+    {
+        public Coche()
+        {
+            ruedas = 4;
+
+            largo = 2300.5;
+
+            ancho = 0.800;
+
+            climatizador = true;
+
+            tapiceria = "Tela";
+
+
+        }
+
+        public Coche(double largoCoche, double anchoCoche)
+        {
+            ruedas = 4;
+
+            largo = largoCoche;
+
+            ancho = anchoCoche;
+
+        }
+        public int getRuedas()
+        {
+            return ruedas;
+        }
+    }
+
+
+    partial class Coche
+    { 
+        public String getInfoCoche()
+        {
+            return "Información del coche: " + "\nRuedas:" + ruedas + "\nLargo:" + largo + "\nAncho:" + ancho;
+        }
+
+        public void setExtras(bool climatizador, String tapiceria)
+        {
+            this.climatizador = climatizador;
+            
+            this.tapiceria = tapiceria;
+
+        }
+        
+        public String getExtras()
+        {
+
+            return "Extras del coche: " + "\nTapiceria:" + tapiceria + "\nClimatizador:" + climatizador;
+        }
+
+        private int ruedas;
+
+        private double largo;
+
+        private double ancho;
+
+        private bool climatizador;
+
+        private String tapiceria;
+
+    }
+
+    
+}   
+ 
